2af7b15137454dcc020adb3aa0f0e86fd2cc9e47
vquynh <ng.viet.quynh@gmail.com>
2023-01-27 22:37:38 +0700

    added setAllowedOrigins

--- src/main/java/de/oncampus/quizlingo/config/WebSocketConfig.java
+++ src/main/java/de/oncampus/quizlingo/config/WebSocketConfig.java
@@ -15,7 +15,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket");
+        registry.addEndpoint("/websocket")
+                .setAllowedOrigins("*");
         registry.addEndpoint("/websocket")
                 .setAllowedOrigins("*").withSockJS();
     }
